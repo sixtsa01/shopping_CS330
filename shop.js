38ae17de7668824af50d11d08dbf6ffcbbd26f6d
Sam Sixta <sixtsa01@luther.edu>
2018-02-27 07:37:23 -0800

    adding

--- shop.js
+++ shop.js
@@ -8,8 +8,9 @@ function add() {
 	let priority = document.querySelector('#prior').value;
 	let price = document.querySelector('#price').value;
 	let mytable = document.querySelector('#list');
-	let lst = [name, quantity, store, section, priority, price]
+	let lst = [name, quantity, store, section, price]
 	let row = document.createElement("tr");
+	row.setAttribute("class", String(priority));
 	let box = document.createElement("input");
 	box.setAttribute("type", "checkbox");
 	row.appendChild(box);
