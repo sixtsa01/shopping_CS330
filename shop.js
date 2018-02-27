fa23160015a3feef66c645f169621014bc7f0e6a
Sam Sixta <sixtsa01@luther.edu>
2018-02-27 06:53:29 -0800

    initial commit

---
+++ shop.js
@@ -0,0 +1,22 @@
+"use strict;"
+
+function add() {
+	let name = document.querySelector('#name').value;
+	let quantity = document.querySelector('#quan').value;
+	let store = document.querySelector('#store').value;
+	let section = document.querySelector('#section').value;
+	let priority = document.querySelector('#prior').value;
+	let price = document.querySelector('#price').value;
+	let mytable = document.querySelector('#list');
+	let lst = [name, quantity, store, section, priority, price]
+	let row = document.createElement("tr");
+	let box = document.createElement("input");
+	box.setAttribute("type", "checkbox");
+	row.appendChild(box);
+	for (l in lst) {
+		let cell = document.createElement("td");
+		cell.innerHTML = lst[l];
+		row.appendChild(cell);
+	}
+	mytable.appendChild(row);
+}
