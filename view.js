16ac3be9d98885d8c923ec98f815aa70ec0d8894
Sam Sixta <sixtsa01@luther.edu>
2018-03-05 17:35:34 -0800

    adding

---
+++ view.js
@@ -0,0 +1,43 @@
+var viewThing = new Subject()
+
+viewThing.subscribe()
+
+
+
+function redrawTable(shoppingList) {
+
+    let tab = document.querySelector("#itemtab");
+
+    for (item of shoppingList.shopList) {
+
+        let row = tab.appendChild(document.createElement("tr"));
+
+        let check = row.appendChild(document.createElement("td"));
+
+        let box = check.appendChild(document.createElement("input"));
+
+        box.setAttribute("type", "checkbox");
+
+        let namedata = row.appendChild(document.createElement("td"));
+
+        namedata.innerHTML = item.getName();
+
+        let qtydata = row.appendChild(document.createElement("td"));
+
+        qtydata.innerHTML = item.getQuantity();
+
+        let storedata = row.appendChild(document.createElement("td"));
+
+        storedata.innerHTML = item.getStore();
+
+        let sectiondata = row.appendChild(document.createElement("td"));
+
+        sectiondata.innerHTML = item.getSection();
+
+        let pricedata = row.appendChild(document.createElement("td"));
+
+        pricedata.innerHTML = item.getPrice();
+
+    }
+
+}
