5a443765602d81de25d70c683c8237d344d4aa41
Sam Sixta <sixtsa01@luther.edu>
2018-03-12 19:57:41 -0500

    working

--- model.js
+++ model.js
@@ -34,6 +34,7 @@ class Subject {
 class Item extends Subject {
 
     constructor(name, store, section, price, quantity, priority) {
+	super()
 
         this.name = name;
 
@@ -75,7 +76,62 @@ class ShoppingList extends Subject{
     addItem(item) {
 
         this.shopList.push(item);
+	localStorage.clear();
+        localStorage.setItem('theList', JSON.stringify(this.shopList));
 	this.publish('newitem', this)
 
+    }
+
+   removeItem(item) {
+        let index = this.shopList.indexOf(item);
+        if (index > -1) {
+            let it = this.shopList.splice(index, 1)
+        }
+        localStorage.clear();
+        localStorage.setItem('theList', JSON.stringify(this.shopList));
+        this.publish('removeditem', this)
+    }
+
+   sortByName() {
+        this.shopList.sort(function(obj1, obj2){
+            var name1 = String(obj1.name).toLowerCase();
+            var name2 = String(obj2.name).toLowerCase();
+            if (name1 < name2) {return -1;}
+            if (name1 > name2) {return 1;}
+            return 0;
+        })
+        this.publish('sorted by name', this)
+    }
+
+    sortByQuantity() {
+        this.shopList.sort(function(obj1, obj2){return obj1.quantity - obj2.quantity})
+        this.publish('sorted by quantity', this)
+    }
+
+    sortByStore() {
+        this.shopList.sort(function(obj1, obj2){
+            var store1 = String(obj1.store).toLowerCase();
+            var store2 = String(obj2.store).toLowerCase();
+            if (store1 < store2) {return -1;}
+            if (store1 > store2) {return 1;}
+            return 0;
+        })
+        this.publish('sorted by store', this)
+    }
+
+    sortBySection() {
+        this.shopList.sort(function(obj1, obj2){
+            var section1 = String(obj1.section).toLowerCase();
+            var section2 = String(obj2.section).toLowerCase();
+            if (section1 < section2) {return -1;}
+            if (section1 > section2) {return 1;}
+            return 0;
+        })
+        this.publish('sorted by section', this)
+    }
+    
+    sortByPrice() {
+        this.shopList.sort(function(obj1, obj2){return obj1.price - obj2.price})
+        this.publish('sorted by price', this)
     }
 }
