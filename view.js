8a95a693abcd79696b4dcb9619720883a866324b
Sam Sixta <sixtsa01@luther.edu>
2018-03-12 19:27:49 -0500

    adding

--- view.js
+++ view.js
@@ -4,45 +4,38 @@ class ShoppingView {
 	}
 
 
-	function redrawTable(shoppingList, msg) {
-		let tab = document.querySelector("#itemList");
-
-    		tab.innerHTML = "";
-
-    		for (item of shoppingList.shopList) {
-
-        		let row = tab.appendChild(document.createElement("tr"));			row.classList.add(item.priority)
+	redrawTable(shoppingList, msg) {
+		let draw = document.getElementById("#item_List");
+   		draw.innerHTML = "";
+    		for (let item of shoppingList.shopList) {
+			let row = draw.appendChild(document.createElement("tr")
+			row.classList.add(item.priority);
 			let box = document.createElement("input")
 			box.type = "checkbox"
 			row.appendChild(box)
 			box.onclick = function() {
-				item.purchased = true;
-				box.parentElement.classList.add("strikeout")
+				if (item.purchased === false) {
+					item.purchased = true;
+					cb.parentElement.classList.add("strikeout")
+               		 }	else {
+                    			item.purchased = false;
+                    			cb.parentElement.classList.remove("strikeout")
+               			 }
 
        			 }
 
-        let namedata = row.appendChild(document.createElement("td"));
-
-        namedata.innerHTML = item.name;
-
-        let qtydata = row.appendChild(document.createElement("td"));
-
-        qtydata.innerHTML = item.quantity;
-
-        let storedata = row.appendChild(document.createElement("td"));
-
-        storedata.innerHTML = item.store;
-
-        let sectiondata = row.appendChild(document.createElement("td"));
-
-        sectiondata.innerHTML = item.section;
-
-        let pricedata = row.appendChild(document.createElement("td"));
-
-        pricedata.innerHTML = item.price;
-
-    		}
-
+	    	let namedata = row.appendChild(document.createElement("td"));
+            	namedata.innerHTML = item.name;
+            	let quantitydata = row.appendChild(document.createElement("td"));
+            	quantitydata.innerHTML = item.quantity;
+            	let storedata = row.appendChild(document.createElement("td"));
+            	storedata.innerHTML = item.store;
+            	let sectiondata = row.appendChild(document.createElement("td"));
+            	sectiondata.innerHTML = item.section;
+            	let pricedata = row.appendChild(document.createElement("td"));
+            	pricedata.innerHTML = item.price;
+        	
+		}
 	}
 
 }
