244aae0abde6f0952a34c62e6161c78d786505eb
Sam Sixta <sixtsa01@luther.edu>
2018-03-12 19:27:20 -0500

    adding

--- controller.js
+++ controller.js
@@ -1,22 +1,52 @@
-var shoppingList = new ShoppingList();
-var thing = new ShoppingView(shoppingList)
+var shoppingModel = new ShoppingList();
+var myView = new ShoppingView(shoppingModel)
+
 
 function clickedon() {
-
-    let name_value = document.querySelector("#name").value;
-
-    let store_value = document.querySelector("#store").value;
-
-    let section_value = document.querySelector("#section").value;
-
-    let price_value = document.querySelector("#price").value;
-
-    let qty_value = document.querySelector("#quantity").value;
-
-    let priority_value = document.querySelector("#priority").value;
-
-    let newItem = new Item(name_value, store_value, section_value, price_value, qty_value, priority_value);
-
+    let name_Val = document.querySelector("#name").value;
+    let store_Val = document.querySelector("#store").value;
+    let section_Val = document.querySelector("#section").value;
+    let price_Val = document.querySelector("#price").value;
+    let qty_Val = document.querySelector("#quantity").value;
+    let priority_Val = document.querySelector("#priority").value;
+    let newItem = new Item(name_Val, store_Val, section_Val, price_Val, qty_Val, priority_Val);
     shoppingList.addItem(newItem);
+}
 
+function reloadTable(itemList) {
+    for (let itemDict of itemList) {
+        let name_Val = itemDict["name"]
+        let store_Val = itemDict["store"]
+        let section_Val = itemDict["section"]
+        let price_Val = itemDict["price"]
+        let quantity_Val = itemDict["quantity"]
+        let priority_Val = itemDict["priority"]
+        let newItem = new Item(name_Val, store_Val, section_Val, price_Val, quantity_Val, priority_Val)
+        shoppingList.addItem(newItem)
+    }
+}
+
+function nameSort() {
+    shoppingList.sortByName()
+    console.log("sort by name")
+}
+
+function quantitySort() {
+    shoppingList.sortByQuantity()
+    console.log("sort by quantity")
+}
+
+function storeSort() {
+    shoppingList.sortByStore()
+    console.log("sort by store")
+}
+
+function sectionSort() {
+    shoppingList.sortBySection()
+    console.log("sort by section")
+}
+
+function priceSort() {
+    shoppingList.sortByPrice()
+    console.log("sort by price")
 }
