4e78e26453438f11ffd78a8b9028c499721f81ef
Sam Sixta <sixtsa01@luther.edu>
2018-03-22 09:11:32 -0500

    change

--- shopping_server.py
+++ shopping_server.py
@@ -5,14 +5,15 @@ app = Flask(__name__)
 
 @app.route('/getlist', methods=['GET'])
 def getList():
-        lst = request,json.get('list')
-        return jsonify(localStorage.getItem(lst))
+        lst = open('list.txt', 'r')
+	return jsonify(file.readlines())
 
 @app.route('/savelist', methods=['GET', 'POST'])
 def saveList():
-        lst = request.json.get('list')
-        items = request.json.get('newItems')
-        localStorage.setItem(lst, items)
-
+        lst = open('list.txt', 'w')
+	lst_2 = request.json.get('list')
+	lst.write(lst_2)
+	lst.close()
+        
 if __name__ == '__main__':
 	app.run()
