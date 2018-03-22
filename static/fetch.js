128dd1e2b67438b97a270e165722407522584cd3
Sam Sixta <sixtsa01@luther.edu>
2018-03-22 14:35:55 -0500

    fetch

--- static/fetch.js
+++ static/fetch.js
@@ -1,1 +1,13 @@
 fetch(`/getList?list=$(this.lstname)`)
+.then(function(response) {
+	console.log(response)
+	return respons.json()
+})
+.catch(error => console.error('Error: ', error))
+.then(function(myJson) {
+	store_list = JSON.parse(myJson)
+	for (let vals of myJson) {
+		let vals = new Items(vals.name, vals.quantity, vals.priority, vals.store, vals.section, vals.price)
+		model.addItem(it)
+	}
+})
